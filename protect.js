de0d577d0ebcaf74a75b8daac1eb3d9a877a3ba3
umeshsir05 <ukbanjare1989@gmail.com>
2025-10-10 15:55:18 +0530

    Update protect.js

--- protect.js
+++ protect.js
@@ -6,23 +6,3 @@ document.addEventListener("contextmenu", function(e){
   alert("Right-click is disabled!");
 });
 
-// 2️⃣ Block common keyboard shortcuts (desktop)
-document.addEventListener("keydown", function(e) {
-  if (
-    e.key === "F12" || 
-    (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === "i") ||
-    (e.ctrlKey && e.key.toLowerCase() === "u") ||
-    (e.ctrlKey && e.key.toLowerCase() === "c") ||
-    (e.ctrlKey && e.key.toLowerCase() === "s")
-  ) {
-    e.preventDefault();
-    alert("Shortcut is disabled!");
-  }
-});
-
-
-
-// 5️⃣ Disable drag (images / links)
-document.addEventListener("dragstart", function(e){
-  e.preventDefault();
-});
